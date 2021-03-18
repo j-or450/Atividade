c027403566ea38d689800e3f2b27d7a5b5415311
j-or450 <you@example.com>
2021-03-18 12:37:30 -0300

    Commit para correão

---
+++ src/um/VariavelMain.java
@@ -0,0 +1,9 @@
+package um;
+
+public class VariavelMain {
+    public static void main(String[] args) {
+        Variavel variavel = new Variavel();
+        variavel.imprimirValores();
+    }
+}
+
